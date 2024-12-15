90a6bae3d6d837cffdc355f70fcdaa595bed32a4
Ayachi <282935885@qq.com>
2024-12-15 15:46:09 -0500

    node_BASE FIXED: made integrate a new for loop alone,

--- ClothSimulation/Headers/Cloth.h
+++ ClothSimulation/Headers/Cloth.h
@@ -187,35 +187,31 @@ public:
 		}
 	}
 
-void node_based_simulation(double timeStep, Vec3 gravity, Ground* ground, Ball* ball) {
+void simulation(double timeStep, Vec3 gravity, Ground* ground, Ball* ball) {
     double total_start = omp_get_wtime();
     double simulation_time = 0.0;
 
     if (enable_parallel) {
-        //#pragma omp parallel for num_threads(num_threads)
+        #pragma omp parallel for num_threads(num_threads)
         for (int i = 0; i < nodes.size(); i++) {
             Node* node = nodes[i];
             
             // Reset forces
-            node->force = Vec3(0.0, -9.8/100, 0.0);
+            //node->force = Vec3(0.0, -9.8/100, 0.0);
 
             // Calculate spring forces
             //std::cout << "connectedSprings: " << node->connectedSprings.size() << std::endl;
             for (Spring* spring : node->connectedSprings) {
                 Vec3 force = spring->computeForce(node);
-                std::cout << "Spring " << i << ": Force = " << force << std::endl;
+                //std::cout << "Spring " << i << ": Force = " << force << std::endl;
                 node->addForce(force);
             }
 
-            std::cout << "Node " << i << ": Position = " << node->position << ", TOTAL Force = " << node->force << std::endl;
-            // Integrate node
-            //node->integrate(timeStep);
-            node->force = Vec3(0.0, 0.0, 0.0);
-            // // Debug output
-            //#pragma omp critical
-            //{
-                //std::cout << "Node " << i << ": Position = " << node->position << ", Velocity = " << node->velocity << std::endl;
-            //}
+        }
+        #pragma omp parallel for num_threads(num_threads)
+        for (int i = 0; i < nodes.size(); i++) {
+            Node* node = nodes[i];
+            node->integrate(timeStep);
 
             // Ground collision
             if (getWorldPos(node).y < ground->position.y) {
@@ -233,27 +229,30 @@ void node_based_simulation(double timeStep, Vec3 gravity, Ground* ground, Ball* 
                 node->velocity = node->velocity * ball->friction;
             }
         }
+
+
     } else {
+        //#pragma omp parallel for num_threads(num_threads)
         for (int i = 0; i < nodes.size(); i++) {
             Node* node = nodes[i];
             
             // Reset forces
-            //node->force = Vec3(0.0, 0.0, 0.0);
+            //node->force = Vec3(0.0, -9.8/100, 0.0);
 
             // Calculate spring forces
+            //std::cout << "connectedSprings: " << node->connectedSprings.size() << std::endl;
             for (Spring* spring : node->connectedSprings) {
                 Vec3 force = spring->computeForce(node);
+                //std::cout << "Spring " << i << ": Force = " << force << std::endl;
                 node->addForce(force);
             }
 
-            std::cout << "Node " << i << ": Position = " << node->position << ", Force = " << node->force << std::endl;
+        }
+        //#pragma omp parallel for num_threads(num_threads)
+        for (int i = 0; i < nodes.size(); i++) {
+            Node* node = nodes[i];
+            node->integrate(timeStep);
 
-            // Integrate node
-            //node->integrate(timeStep);
-
-            // Debug output
-            //std::cout << "Node " << i << ": Position = " << node->position << ", Velocity = " << node->velocity << std::endl;
-            //std::cout << "Node " << i << ": Position = " << node->position << ", Force = " << node->force << std::endl;
             // Ground collision
             if (getWorldPos(node).y < ground->position.y) {
                 node->position.y = ground->position.y - clothPos.y + 0.01;
@@ -304,7 +303,7 @@ void node_based_simulation(double timeStep, Vec3 gravity, Ground* ground, Ball* 
 }
 
 //simulation
-void simulation(double timeStep, Vec3 gravity, Ground* ground, Ball* ball) {
+void senqu_simulation(double timeStep, Vec3 gravity, Ground* ground, Ball* ball) {
         // init
         double total_start = omp_get_wtime();
         double simulation_time = 0.0;
